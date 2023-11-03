39a1320b1928a2a90f3ab5db35c0e70ba1422d69
Eric Chang <eric141822@gmail.com>
2023-11-02 23:45:36 -0500

    better exception handling, loop added

--- ftoc/src/main.rs
+++ ftoc/src/main.rs
@@ -3,26 +3,54 @@ fn main() {
     println!("Do you want to convert: ");
     println!("1. Farenheit to Celsius");
     println!("2. Celsius to Farenheit");
-    let mut choice: String = String::new();
+    println!("3. Exit");
+    loop {
+        let mut choice: String = String::new();
 
-    io::stdin().read_line(&mut choice).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
 
-    let choice: u32 = choice.trim().parse().expect("Please type a number!");
-
-    if choice == 1 {
-        let mut farenheit: String = String::new();
-        println!("Enter the temperature in Farenheit: ");
-        io::stdin().read_line(&mut farenheit).expect("Failed to read line");
-        let farenheit: f64 = farenheit.trim().parse().expect("Please type a number!");
-        println!("{} Farenheit is {} Celsius", farenheit, farenheit_to_celsius(farenheit as f64));
-    } else if choice == 2 {
-        let mut celsius: String = String::new();
-        println!("Enter the temperature in Celsius: ");
-        io::stdin().read_line(&mut celsius).expect("Failed to read line");
-        let celsius: f64 = celsius.trim().parse().expect("Please type a number!");
-        println!("{} Celsius is {} Farenheit", celsius, celcius_to_farenheit(celsius as f64));
-    } else {
-        println!("Please enter a valid choice (1 or 2)");
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid choice.");
+                continue;
+            }
+        };
+        match choice {
+            1 => {
+                let mut farenheit: String = String::new();
+                println!("Enter the temperature in Farenheit: ");
+                io::stdin()
+                    .read_line(&mut farenheit)
+                    .expect("Failed to read line");
+                let farenheit: f64 = farenheit.trim().parse().expect("Please type a number!");
+                println!(
+                    "{} Farenheit is {} Celsius",
+                    farenheit,
+                    farenheit_to_celsius(farenheit as f64)
+                );
+            }
+            2 => {
+                let mut celsius: String = String::new();
+                println!("Enter the temperature in Celsius: ");
+                io::stdin()
+                    .read_line(&mut celsius)
+                    .expect("Failed to read line");
+                let celsius: f64 = celsius.trim().parse().expect("Please type a number!");
+                println!(
+                    "{} Celsius is {} Farenheit",
+                    celsius,
+                    celcius_to_farenheit(celsius as f64)
+                );
+            }
+            3 => break,
+            _ => {
+                println!("Please enter a valid choice");
+                continue;
+            }
+        }
     }
 }
 
